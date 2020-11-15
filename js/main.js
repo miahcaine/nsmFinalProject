18c202d5f96b3f8145f8059678cb0b4eedb92990
Nneka Okolo <you@example.com>
2020-11-14 21:34:54 -0500

    Victims Vis

--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
+// init global Data array
+let globalData = []
+
+// init global variables & switches
+let myVictimsVis;
+
+
 function showNextVis(sectionID) {
 
     $("#" + sectionID).fadeIn();
     document.getElementById(sectionID).scrollIntoView({ behavior: 'smooth', block: 'start', });
+}
+
+function showVis4() {
+    $("#victims-sec").fadeIn();
+    document.getElementById("victims-sec").scrollIntoView({ behavior: 'smooth', block: 'start', });
+    myVictimsVis = new VictimsVis('vis-4', globalData);
+}
+
+// load data using promises
+let promises = [
+    d3.csv("sf_data/stops/2003.csv"),
+    // d3.csv("sf_data/stops/2004.csv"),
+    // d3.csv("sf_data/stops/2005.csv"),
+    // d3.csv("sf_data/stops/2006.csv"),
+    // d3.csv("sf_data/stops/2007.csv"),
+    // d3.csv("sf_data/stops/2008.csv"),
+    // d3.csv("sf_data/stops/2009.csv"),
+    // d3.csv("sf_data/stops/2010.csv"),
+    // d3.csv("sf_data/stops/2011.csv"),
+    // d3.csv("sf_data/stops/2012.csv"),
+    // d3.csv("sf_data/stops/2013.csv"),
+    // d3.csv("sf_data/stops/2014.csv"),
+    // d3.csv("sf_data/stops/2015.csv"),
+    // d3.csv("sf_data/stops/2016.csv"),
+];
+
+Promise.all(promises)
+    .then( function(data){ initMainPage(data) })
+    .catch( function (err){console.log(err)} );
+
+function initMainPage(dataArray) {
+    globalData = dataArray
+    console.log(dataArray)
+    // myVictimsVis = new VictimsVis('vis-1', dataArray);
+}
+
+function updateVis4() {
+    myVictimsVis.updateVis()
 }
--- js/victims.js
+++ js/victims.js
@@ -19,15 +19,15 @@ class VictimsVis {
 
         vis.margin = {top: 20, right: 20, bottom: 20, left: 20};
         vis.width = $("#" + vis.parentElement).width() - vis.margin.left - vis.margin.right;
-        vis.height = ($("#" + vis.parentElement).height() / 2) - vis.margin.top - vis.margin.bottom;
+        vis.height = ($("#" + vis.parentElement).height()) - vis.margin.top - vis.margin.bottom;
 
         // init drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
             .attr("width", vis.width + vis.margin.left + vis.margin.right)
             .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
             .attr("class", "bubble")
-            .append('g')
-            .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
+
+        vis.count = 0
 
         this.wrangleData();
     }
@@ -38,30 +38,90 @@ class VictimsVis {
         // init final data structure in which both data sets will be merged into
         vis.groupedData = []
 
-        vis.countByRace = {
-            "male": 0,
-            "female": 0,
-            "unknwon": 0
+        vis.countBySex = {}
+        vis.countByRace = {}
+        vis.countByAge = {}
+        vis.countByAgeRange = {
+            "M": 0,
+            "20": 0,
+            "30": 0,
+            "40": 0,
+            "50": 0,
+            "60": 0,
+            "Z": 0,
         }
+        vis.countByBuild = {}
+        vis.totalVictims = 0
 
         for(let i=0; i < vis.dataSets.length; i++) {
             let dataByYear = vis.dataSets[i]
 
             for(let j=0; j < dataByYear.length; j++) {
 
-                vis.groupedData.push(
-                    {
-                        weight: +dataByYear[j].weight,
-                        sex: dataByYear[j].sex,
-                        race: dataByYear[j].race,
-                        height_ft: +dataByYear[j].ht_feet,
-                        height_inch: +dataByYear[j].ht_inch,
-                        hair_color: dataByYear[j].haircolr,
-                        eye_color: dataByYear[j].eyecolor,
-                        build: dataByYear[j].build,
-                        age: +dataByYear[j].age
-                    }
-                )
+                // vis.groupedData.push(
+                //     {
+                //         weight: +dataByYear[j].weight,
+                //         sex: dataByYear[j].sex,
+                //         race: dataByYear[j].race,
+                //         height_ft: +dataByYear[j].ht_feet,
+                //         height_inch: +dataByYear[j].ht_inch,
+                //         hair_color: dataByYear[j].haircolr,
+                //         eye_color: dataByYear[j].eyecolor,
+                //         build: dataByYear[j].build,
+                //         age: +dataByYear[j].age
+                //     }
+                // )
+
+                let sex = dataByYear[j].sex
+                if (vis.countBySex.hasOwnProperty(sex)) {
+                    vis.countBySex[sex] += 1
+                } else {
+                    vis.countBySex[sex] = 1
+                }
+
+                let race = dataByYear[j].race
+                if (vis.countByRace.hasOwnProperty(race)) {
+                    vis.countByRace[race] += 1
+                } else {
+                    vis.countByRace[race] = 1
+                }
+
+                let age = +dataByYear[j].age
+                switch(true) {
+                    case age == 0:
+                        vis.countByAgeRange["Z"] += 1
+                        break;
+                    case age < 18 :
+                        vis.countByAgeRange["M"] += 1
+                        break;
+                    case age < 30:
+                        vis.countByAgeRange["20"] += 1
+                        break;
+                    case age < 40:
+                        vis.countByAgeRange["30"] += 1
+                        break;
+                    case age < 50:
+                        vis.countByAgeRange["40"] += 1
+                        break;
+                    case age < 60:
+                        vis.countByAgeRange["50"] += 1
+                        break;
+                    case age < 101:
+                        vis.countByAgeRange["60"] += 1
+                        break;
+                    default:
+                        vis.countByAgeRange["Z"] += 1
+                      break;
+                }
+
+                let build = dataByYear[j].build
+                if (vis.countByBuild.hasOwnProperty(build)) {
+                    vis.countByBuild[build] += 1
+                } else {
+                    vis.countByBuild[build] = 1
+                }
+
+                vis.totalVictims += 1
 
 
 
@@ -69,164 +129,284 @@ class VictimsVis {
 
         }
 
-        console.log(vis.groupedData)
+        // some data cleaning
+        vis.countBySex["Z"] += vis.countBySex[" "]
+        vis.countBySex[" "] = 0
 
+        vis.countByRace["Z"] += vis.countByRace[" "]
+        vis.countByRace[" "] = 0
+
+        vis.countByBuild["Z"] += vis.countByBuild[" "]
+        vis.countByBuild[" "] = 0
+
+        
+
+        vis.scale = d3.scaleTime()
+            .range([0, 1000])
+            .domain([0, vis.totalVictims])
+
+        vis.sexDataSet = []
+        let c1 = 0
+        for (let property in vis.countBySex) {
+            if (property == " ") {
+                break;
+            }
+            let count = Math.round(vis.scale(vis.countBySex[property]))
+            vis.countBySex[property] = count
+            let countArr = []
+            for (let i = 0; i < count; i++ ) {
+                countArr.push({"sex": property})
+                // vis.sexDataSet.push({"children": countArr})
+            }
+            vis.sexDataSet.push({"children": countArr})
+            
+            c1 += 1
+        }
+
+        console.log("here");
+        console.log(vis.sexDataSet)
+
+        vis.raceDataSet = []
+        vis.raceDataSet2 = []
+        c1 = 0
+        for (let property in vis.countByRace) {
+            if (property == " ") {
+                break;
+            }
+            let count = Math.round(vis.scale(vis.countByRace[property]))
+            vis.countByRace[property] = count
+            let countArr = []
+            for (let i = 0; i < count; i++ ) {
+                countArr.push({"build": property})
+                vis.raceDataSet2.push({"group": c1})
+            }
+            vis.raceDataSet.push({"children": countArr})
+            c1 += 1
+        }
+
+       vis.ageRangeDataset = []
+        for (let property in vis.countByAgeRange) {
+            if (property == " ") {
+                break;
+            }
+            let count = Math.round(vis.scale(vis.countByAgeRange[property]))
+            vis.countByAgeRange[property] = count
+            let countArr = []
+            for (let i = 0; i < count; i++ ) {
+                countArr.push({"build": property})
+            }
+            vis.ageRangeDataset.push({"children": countArr})
+        }
+
+        vis.buildDataSet = []
+        for (let property in vis.countByBuild) {
+            if (property == " ") {
+                break;
+            }
+            let count = Math.round(vis.scale(vis.countByBuild[property]))
+            vis.countByBuild[property] = count
+            let countArr = []
+            for (let i = 0; i < count; i++ ) {
+                countArr.push({"build": property})
+            }
+            vis.buildDataSet.push({"children": countArr})
+        }
+
+        vis.nodes = []
+        vis.nodes1 = []
+
+        for (let i = 0; i < 1000; i++) {
+            vis.nodes.push({"victims": 0})
+        }
+
+        console.log(vis.nodes)
 
         vis.updateVis()
 
     }
 
 
+
+
     updateVis(){
         let vis = this;
 
-        let dataset = {
-            "children": vis.groupedData.slice(0,10)
-        };
+        vis.count += 1
 
-        let diameter = 450;
+        let dataset = {}
 
+        console.log(vis.count)
+        if (vis.count == 1) {
+            dataset = {
+                "children": vis.nodes
+            };
+        } else if (vis.count == 2) {
+            dataset = {
+                "children": vis.sexDataSet
+            };
+        } else if (vis.count == 3) {
+            dataset = {
+                "children": vis.raceDataSet
+            };
+        } else if (vis.count == 4) {
+            dataset = {
+                "children": vis.ageRangeDataset
+            };
+        } else if (vis.count == 5) {
+            dataset = {
+                "children": vis.buildDataSet
+            };
+        } else {
+            
+            dataset = {
+                "children": vis.nodes
+            };
+
+        }
+        
+
+        console.log(dataset)
+        let diameter = 550;
+
+        console.log(vis.width)
+        console.log(vis.height)
         let bubble = d3.pack(dataset)
             .size([diameter, diameter])
             .padding(10);
 
-        let nodes2 = d3.hierarchy(dataset)
+        let nodes = d3.hierarchy(dataset)
             .sum(function(d) { return 1; });
-        //
-        // let node = vis.svg.selectAll(".node")
-        //     .data(bubble(nodes).descendants())
-        //     .enter()
-        //     .filter(function(d){
-        //         return  !d.children
-        //     })
-        //     .append("g")
+        
+        let node = vis.svg.selectAll("circle")
+            .data(bubble(nodes).descendants())
+
+                
+        node.enter()
+            .append("circle")
+            .attr("class", "node")
+            .merge(node)
+            .transition()
+            .duration(1000)
+            .filter(function(d){
+                return  !d.children
+            })
+            .attr("cx", d=> d.x)
+            .attr("cy", d=> d.y)
+            .attr("r", function(d) {
+                return d.r;
+            })
+            .style("fill", function(d,i) {
+                return "black";
+            });
+    
+
+        node.exit().remove()
+
+        // let dataset = []
+        // let data = [{ "name": "A", "group": 1 }, { "name": "B", "group": 1 }, { "name": "C", "group": 1 }, { "name": "D", "group": 1 }, { "name": "E", "group": 1 }, { "name": "F", "group": 1 },
+        // { "name": "G", "group": 2 }, { "name": "H", "group": 2 }, { "name": "I", "group": 2 }, { "name": "J", "group": 2 }, { "name": "K", "group": 2 }, { "name": "L", "group": 2 },
+        // { "name": "M", "group": 3 }, { "name": "N", "group": 3 }, { "name": "O", "group": 3 }]
+
+        // console.log(vis.count)
+        // if (vis.count == 1) {
+        //     dataset = vis.nodes
+        //     data = data = [{ "name": "A", "group": 3 }, { "name": "B", "group": 2 }, { "name": "A", "group": 1 }]
+
+        // // } else if (vis.count == 2) {
+        // //     dataset = {
+        // //         "children": vis.sexDataSet
+        // //     };
+        // // } else if (vis.count == 3) {
+        // //     dataset = {
+        // //         "children": vis.raceDataSet
+        // //     };
+        // // } else if (vis.count == 4) {
+        // //     dataset = {
+        // //         "children": vis.ageRangeDataset
+        // //     };
+        // // } else if (vis.count == 5) {
+        // //     dataset = {
+        // //         "children": vis.buildDataSet
+        // //     };
+        // } else {
+        //     console.log(vis.raceDataSet2)
+        //     dataset = vis.raceDataSet2
+        //     data = [{ "name": "C", "group": 3 }, { "name": "B", "group": 2 }, { "name": "A", "group": 1 }]
+
+        // }
+
+        
+
+        // // A scale that gives a X target position for each group
+        // let x = d3.scaleOrdinal()
+        //     .domain([1, 2, 3, 4, 5, 6, 7])
+        //     .range([50, 150, 250, 50, 150, 250, 350])
+        
+        // let y = d3.scaleOrdinal()
+        //     .domain([1, 2, 3, 4, 5, 6, 7])
+        //     .range([vis.height * 0.25, vis.height * 0.25, vis.height * 0.25, vis.height * 0.75, vis.height * 0.75, vis.height * 0.75, vis.height * 0.5])
+
+        // // A color scale
+        // let color = d3.scaleOrdinal()
+        //     .domain([1, 2, 3, 4, 5, 6, 7])
+        //     .range([ "#F8766D", "#00BA38", "#619CFF", "blue", "black", "pink", "yellow"])
+
+        // // let node = vis.svg.append("g")
+        // //     .selectAll("circle")
+        // //     .data(data)
+
+        // // node.enter()
+        // //     .append("circle")
+        // //     // .attr("class", "node")
+        // //     // .merge(node)
+        // //     .attr("r", 5)
+        // //     .attr("cx", vis.width / 2)
+        // //     .attr("cy", vis.height / 2)
+        // //     .style("fill", function(d){ return color(d.group)})
+        // //     .style("fill-opacity", 0.8)
+        // //     .attr("stroke", "black")
+        // //     .style("stroke-width", 4)
+
+        // // // node.exit().remove()
+
+        // let node = vis.svg
+        //     .selectAll("circle")
+        //     .data(dataset)
+
+        // node.enter()
+        //     .append("circle")
         //     .attr("class", "node")
-        //     .attr("transform", function(d) {
-        //         return "translate(" + d.x + "," + d.y + ")";
+        //     .merge(node)
+        //     .attr("r", 3)
+        //     .attr("cx", vis.width / 2)
+        //     .attr("cy", vis.height / 2)
+        //     .style("fill", function(d){ return color(d.group)})
+        //     .style("fill-opacity", 0.8)
+        //     .attr("stroke", "black")
+        //     .style("stroke-width", 4)
+
+        // node.exit().remove
+            
+
+        // // Features of the forces applied to the nodes:
+        // var simulation = d3.forceSimulation()
+        //     .force("x", d3.forceX().strength(0.5).x( function(d){ return x(d.group) } ))
+        //     .force("y", d3.forceY().strength(0.1).y(  function(d){ return y(d.group) } ))
+        //     .force("center", d3.forceCenter().x(vis.width / 2).y(vis.height / 2)) // Attraction to the center of the svg area
+        //     .force("charge", d3.forceManyBody().strength(0)) // Nodes are attracted one each other of value is > 0
+        //     .force("collide", d3.forceCollide().strength(.1).radius(5).iterations(1)) // Force that avoids circle overlapping
+
+        // // Apply these forces to the nodes and update their positions.
+        // // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+        // simulation
+        //     .nodes(dataset)
+        //     .on("tick", function(d){
+        //         node
+        //             .attr("cx", function(d){ return d.x; })
+        //             .attr("cy", function(d){ return d.y; })
         //     });
-        //
-        //
-        // node.append("circle")
-        //     .attr("r", function(d) {
-        //         return d.r;
-        //     })
-        //     .style("fill", function(d,i) {
-        //         return "black";
-        //     });
 
-        let time_so_far = 0;
-        let nodes = bubble(nodes2).descendants()
 
-        // Circle for each node.
-        const circle = vis.svg.append("g")
-            .selectAll("circle")
-            .data(nodes)
-            .join("circle")
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y)
-            .attr("fill", d => d.color);
-
-        // Ease in the circles.
-        circle.transition()
-            .delay((d, i) => i * 5)
-            .duration(800)
-            .attrTween("r", d => {
-                const i = d3.interpolate(0, d.r);
-                return t => d.r = i(t);
-            });
-
-        const simulation = d3.forceSimulation(nodes)
-            .force("x", d => d3.forceX(d.x))
-            .force("y", d => d3.forceY(d.y))
-            .force("cluster", forceCluster())
-            .force("collide", forceCollide())
-            .alpha(.09)
-            .alphaDecay(0);
-
-        simulation.on("tick", () => {
-            circle
-                .attr("cx", d => d.x)
-                .attr("cy", d => d.y)
-                .attr("fill", d => "blue");
-        });
-
-        function timer() {
-            nodes.forEach(function (o, i) {
-                o.timeleft -= 1;
-                if (o.timeleft == 0 && o.istage < o.stages.length - 1) {
-                    // Decrease count for previous group.
-                    groups[o.group].cnt -= 1;
-                    // Update current node to new group.
-                    o.istage += 1;
-                    o.group = o.stages[o.istage].grp;
-                    o.timeleft = o.stages[o.istage].duration;
-                    // Increment count for new group.
-                    groups[o.group].cnt += 1;
-                }
-            });
-            // Increment time.
-            time_so_far += 1;
-            d3.select("#timecount .cnt").text(time_so_far);
-            // Update counters.
-            vis.svg.selectAll('.grpcnt').text(d => groups[d].cnt);
-            // Do it again.
-            d3.timeout(timer, 500);
-        } // @end timer()
-
-        // Start things off after a few seconds.
-        d3.timeout(timer, 2000);
-
-        function forceCluster() {
-            const strength = .15;
-            let nodes;
-
-            function force(alpha) {
-                const l = alpha * strength;
-                for (const d of nodes) {
-                    d.vx -= (d.x) * l;
-                    d.vy -= (d.y) * l;
-                }
-            }
-            force.initialize = _ => nodes = _;
-
-            return force;
-        }
-
-        function forceCollide() {
-            const alpha = 0.2; // fixed for greater rigidity!
-            const padding1 = 10; // separation between same-color nodes
-            const padding2 = 50; // separation between different-color nodes
-            let nodes;
-            let maxRadius;
-
-            function force() {
-                const quadtree = d3.quadtree(nodes, d => d.x, d => d.y);
-                for (const d of nodes) {
-                    const r = d.r + maxRadius;
-                    const nx1 = d.x - r, ny1 = d.y - r;
-                    const nx2 = d.x + r, ny2 = d.y + r;
-
-                    quadtree.visit((q, x1, y1, x2, y2) => {
-                        if (!q.length) do {
-                            if (q.data !== d) {
-                                const r = d.r + q.data.r + (d.group === q.data.group ? padding1 : padding2);
-                                let x = d.x - q.data.x, y = d.y - q.data.y, l = Math.hypot(x, y);
-                                if (l < r) {
-                                    l = (l - r) / l * alpha;
-                                    d.x -= x *= l, d.y -= y *= l;
-                                    q.data.x += x, q.data.y += y;
-                                }
-                            }
-                        } while (q = q.next);
-                        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-                    });
-                }
-            }
-
-            force.initialize = _ => maxRadius = d3.max(nodes = _, d => d.r) + Math.max(padding1, padding2);
-
-            return force;
-        }
     }
 
 
