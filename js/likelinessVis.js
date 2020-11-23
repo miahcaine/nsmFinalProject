05dadf24e341cf88387bdf30351fc4534946e1d7
Miah Caine <miahc@Miahs-MacBook-Pro.local>
2020-11-22 23:32:15 -0500

    vis 5 and 6 partially done

--- js/likelinessVis.js
+++ js/likelinessVis.js
@@ -1,9 +1,156 @@
 class LikelinessVis {
-  constructor(parentElement, data) {}
+  constructor(parentElement, data) {
+    this.parentElement = parentElement;
+    this.data = data;
 
-  initVis() {}
+    this.race = "";
+    this.age = "";
+    this.sex = "";
+    this.build = "";
 
-  wrangleData() {}
+    this.sqColor = d3.scaleSequential(d3.interpolatePurples);
 
-  updateVis() {}
+    console.log(this.data);
+    this.initVis();
+  }
+
+  initVis() {
+    let vis = this;
+    vis.margin = { top: 0, bottom: 10, left: 20, right: 20 };
+    vis.width =
+      $(`#${vis.parentElement}`).width() + vis.margin.left + vis.margin.right;
+    vis.height =
+      $(`#${vis.parentElement}`).height() + vis.margin.top + vis.margin.bottom;
+
+    vis.svg = d3
+      .select(`#${vis.parentElement}`)
+      .append("svg")
+      .attr("width", vis.width)
+      .attr("height", vis.height);
+
+    vis.wrangleData(vis.race, vis.age, vis.sex, vis.build, false);
+  }
+
+  wrangleData(race, age, sex, build, changed) {
+    
+    let vis = this;
+    vis.changed = changed;
+    vis.ageObj = {
+      0: [12, 17],
+      1: [18, 30],
+      2: [31, 45],
+      3: [46, 60],
+      4: [61, 80]
+    };
+    vis.stopPerc = 0;
+    // let fullData = {};
+    let totalStopCnt = 0;
+    let matchingStopCnt = 0;
+
+    vis.struct = Array.from({ length: 10 }, () =>
+      Array.from({ length: 10 }, () => 0)
+    );
+    // console.log(vis.struct);
+    if (changed) {
+      vis.race = race;
+      vis.ageLow = vis.ageObj[age][0];
+      vis.ageHi = vis.ageObj[age][1];
+      vis.sex = sex;
+      vis.build = build;
+      // console.log(race, age, sex, build);
+
+      for (var i = 0; i < vis.data.length; i++) {
+        for (var j = 0; j < vis.data[i].length; j++) {
+          if (
+            vis.data[i][j].race == vis.race &&
+            vis.data[i][j].sex == vis.sex &&
+            vis.data[i][j].age >= vis.ageLow &&
+              vis.data[i][j].age <= vis.ageHi &&
+            vis.data[i][j].build == vis.build
+          ) {
+            matchingStopCnt++;
+          }
+        }
+        totalStopCnt += vis.data[i].length;
+      }
+      vis.stopPerc = (100 * (matchingStopCnt / totalStopCnt)).toFixed(); // make whole
+      $("#likeliness-perc").text(
+        `Every ${vis.stopPerc} out of every 100 stops have this description.`
+      );
+    }
+
+    let counter = 0;
+    for (var i = 0; i < 10; i++) {
+      for (var j = 0; j < 10; j++) {
+        if (counter < vis.stopPerc) {
+          vis.struct[i][j] = 1;
+          counter++;
+        }
+      }
+    }
+    vis.updateVis();
+  }
+
+  updateVis() {
+    let vis = this;
+
+    vis.padding = 15;
+    vis.boxSz = 30;
+    if (vis.changed){
+      vis.svg.selectAll("g").data(vis.struct);
+    }
+    let row = vis.svg
+      .selectAll("g")
+      .data(vis.struct)
+      .enter()
+      .append("g")
+      .attr(
+       
+        "transform",
+        (d, i) =>{
+          console.log("the data is", vis.struct);
+          return `translate(${vis.margin.left}, ${i * (vis.boxSz + vis.padding) +
+            vis.margin.top})`
+
+        }
+      );
+
+    row
+      .selectAll("rect")
+      .data(function(d) {
+        console.log("data loading", d);
+        return d;
+      })
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => {
+        return i * (vis.boxSz + vis.padding);
+      })
+      .attr("width", vis.boxSz)
+      .attr("height", vis.boxSz)
+      .attr("fill", (d, i) => {
+        console.log(" 1 data in the row is", d);
+        if (d[i] == 1) {
+          return "red";
+        } else {
+          return "gray";
+        }
+      })
+      .merge(row)
+      .attr("x", (d, i) => {
+        return i * (vis.boxSz + vis.padding);
+      })
+      .attr("width", vis.boxSz)
+      .attr("height", vis.boxSz)
+      .attr("fill", (d, i) => {
+        console.log(" 2 data in the row is", d);
+        if (d[i] == 1) {
+          return "red";
+        } else {
+          return "gray";
+        }
+      });
+
+    row.exit().remove();
+  }
 }
